d1eba73c053415bc3fe938d1af6d30afee81daeb
milkBread01 <guzkevin@gmail.com>
2025-04-15 19:34:40 -0400

    javascript popup and submission page added

---
+++ popUp.js
@@ -0,0 +1,13 @@
+// formHandler.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
+
+    if (form) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            alert("Form submitted successfully!");
+            window.location.href = "submission.html"; 
+        });
+    }
+});
